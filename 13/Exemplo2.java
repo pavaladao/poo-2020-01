8077e232accde07f7c2c8cfab0ed2f3b3fd34d3c
pavaladao <61844265+pavaladao@users.noreply.github.com>
2020-10-13 18:21:51 -0300

    Add files via upload

---
+++ 13/Exemplo2.java
@@ -0,0 +1,9 @@
+public class Exemplo2 {
+    public static void main(String[] args) {
+        double myDouble = 9.87; 
+        int myInt = (int) myDouble;
+
+        System.out.println(myInt);      // Saída 9
+        System.out.println(myDouble);   // Saída 9.87
+     }
+}
---
+++ 13/Exemplo3.java
@@ -0,0 +1,11 @@
+public class Exemplo3{
+    int x = 5;
+    int y = 7;
+    float mediaXY = (x+y)/2;
+    public static void main(String[] args) {
+      Exemplo3 myObj = new Exemplo3();
+      System.out.println(myObj.x);
+      System.out.println(myObj.y);
+      System.out.println(myObj.mediaXY);
+    }
+  }
